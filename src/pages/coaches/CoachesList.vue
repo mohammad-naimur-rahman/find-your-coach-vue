d2f3eea4c2eca732196fed2271d9167048f367af
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-07-13 03:50:34 +0600

    a complete vue app

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,85 @@
+<template>
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
+  <section>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button v-if="!isCoach" link to="/register"
+          >Register as Coach</base-button
+        >
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches in the list</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
+
+export default {
+  components: {
+    CoachItem,
+    CoachFilter
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true
+      }
+    };
+  },
+  computed: {
+    ...mapGetters('coaches', ['coaches', 'hasCoaches', 'isCoach']),
+    filteredCoaches() {
+      const allCoaches = this.coaches;
+      return allCoaches.filter(coach => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
+    }
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
